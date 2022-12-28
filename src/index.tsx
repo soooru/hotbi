40ee4463b6cc92341f98e6fdbe939c6b806710a3
soooru <threequaters9@gmail.com>
2022-12-28 15:10:01 +0900

    feat: 질문 페이지 결과 도출 함수 추가

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,13 @@ import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  //<React.StrictMode>
+  <BrowserRouter>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </BrowserRouter>
+  //</React.StrictMode>
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -35,7 +35,7 @@ const AnswerBox = styled.div`
 export default function Question() {
   const [questionOrder, setOrderNum] = useState<number>(0)
   const [questionList, setQuestionList] = useState<questionType[]>([])
-  const [resultList, setResultList] = useState<any[]>([])
+  const [result, setResult] = useState<any>({})
 
   useEffect(() => {
     //문제를 가져오는 함수
@@ -44,8 +44,6 @@ export default function Question() {
         let res = await axios.get('/questions.json')
         let { data } = res.data
         setQuestionList(data)
-
-        console.log('questionList1', data)
       } catch (error) {
         console.log(error)
         setQuestionList([])
@@ -59,14 +57,9 @@ export default function Question() {
   const nextStep = (score: string) => {
     try {
       if (questionOrder < 7) {
-        console.log('결과를 계산한다.')
         calculateQuestion(score)
       } else {
         caculateResult()
-        console.log(
-          '모든 문제가 끝났다. 계산한 결과로 result페이지로 이동한다.'
-        )
-        navigate('/result/intp')
       }
     } catch (error) {
       navigate('/notfound')
@@ -75,19 +68,47 @@ export default function Question() {
 
   //결과를 계산하는 함수
   const calculateQuestion = (score: string) => {
-    const newElement: string[] = score.split(',').map((v) => v.trim())
-    setResultList((current) => [...current, newElement])
+    const newElement: string[] = score.split(',').map((v: string) => v.trim())
+    newElement.forEach((item: string) => {
+      setResult((current: any) => {
+        current[item] = (current[item] || 0) + 1
+        return { ...current }
+      })
+    })
+
     setOrderNum(questionOrder + 1)
   }
 
   //결과를 도출해내는 함수
   const caculateResult = () => {
-    console.log('caculateResult')
-    //리듀스 쓰면 한번일듯.
-    //I와 E의 개수
-    //S와 N의 개수
-    //T와 F의 개수
-    //P와 J의 개수
+    //최종 결과값 구하기
+    const requestCharactor = [
+      ['I', 'E'],
+      ['S', 'N'],
+      ['T', 'F'],
+      ['P', 'J'],
+    ]
+    let resultMBTI = ''
+
+    requestCharactor.forEach((compareItem) => {
+      const firstStr = compareItem[0]
+      const secondStr = compareItem[1]
+      let resultStr = compareItem[0]
+      if (!result[firstStr] && result[secondStr]) {
+        resultStr = secondStr
+      }
+      if (
+        result[firstStr] &&
+        result[secondStr] &&
+        result[firstStr] < result[secondStr]
+      ) {
+        resultStr = secondStr
+      }
+
+      resultMBTI += resultStr
+    })
+
+    navigate(`/result/${resultMBTI}`)
   }
 
   return (
@@ -108,7 +129,6 @@ export default function Question() {
             </button>
           ))}
         </AnswerBox>
-        <div>resultList?{resultList}</div>
       </div>
     </>
   )

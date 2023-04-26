5e994578d8c0a8006b73493b48517309b61a5bcb
soooru <threequaters9@gmail.com>
2023-04-27 02:11:20 +0900

    chore:미사용 이미지 삭제

--- src/components/HelmetComponents.tsx
+++ src/components/HelmetComponents.tsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useContext } from 'react'
-import ogImage from 'assets/images/npc-sol.jpg'
+import ogImage from 'assets/images/ic_ball.png'
 
 import { AppInfo } from 'contexts/AppInfo'
 export default function HelmetComponents(props: { title: string }) {
